b878244ddcb6c7d3e0f0ac07421502174053cc01
wrathsc <127319280+wrathsc@users.noreply.github.com>
2023-03-22 13:44:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+
+   
+    
+    System.out.println(" liczbe a wynosi 2");
+    double a =2;
+    System.out.println(" liczbe b wynosi 8");
+    double b = 8;
+
+      double sum = a + b;
+    System.out.println("suma wunosi " + sum);
+      double roz= a - b;
+    System.out.println(" różnica wynosi " + roz);
+      double ilocz= a * b;
+    System.out.println("iloczyn wynosi " + ilocz);
+      double ilor= a / b;
+    System.out.println(" iloraz wynosi " + ilor);
+    
+  }
+}
